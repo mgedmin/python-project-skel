30a2d6be8cfc3e89bd6496541d9ba1168b4e005f
Marius Gedminas <marius@gedmin.as>
2020-12-01 20:00:47 +0200

    Smarter 'make help'
    * Fix case when commands are declared above the first section, e.g.
    
         foo:  ##: do foo
         ##: also
         bar:  ##: do bar
    
      now produces
    
         make foo   # do foo
    
         also
    
         make bar   # do bar
    
      instead of
    
         make foo   # do foo
         also
    
         make bar   # do bar
    
    * Fix empty section titles:
    
         foo:  ##: do foo
         ##:
         bar:  ##: do bar
         ##:
         baz:  ##: do baz
    
      now produces
    
         make foo   # do foo
    
         make bar   # do bar
    
         make baz   # do baz
    
      instead of
    
         make foo   # do foo
    
         make bar   # do bar
    
         make baz   # do baz
    
    * Document every target on a separate line, e.g.
    
         foo bar:  ##: do thing
    
      now produces
    
         make foo   # do thing
         make bar   # do thing
    
      instead of
    
         make foo bar  # do thing

--- help.mk
+++ help.mk
@@ -1,4 +1,4 @@
-# help.mk version 2.0 (2020-10-11)
+# help.mk version 2.1 (2020-12-01)
 #
 # Helpful Makefile rules for 'make help'
 # https://github.com/mgedmin/python-project-skel
@@ -14,6 +14,7 @@ HELP_SECTION_SEP = "\n"
 help:
 	@grep -Eh -e '^[a-zA-Z0-9_ -]+:.*?##: .*$$' -e '^##:' $(MAKEFILE_LIST) \
 	    | awk 'BEGIN {FS = "(^|:[^#]*)##: "; section=""}; \
-	          /^##:/ {printf "%s%s\n%s", section, $$2, $(HELP_SECTION_SEP); section=$(HELP_SECTION_SEP)} \
-	          /^[^#]/ {printf "%s\033[36m%-$(HELP_WIDTH)s\033[0m%s%s\n", \
-	                   $(HELP_INDENT), $(HELP_PREFIX) $$1, $(HELP_SEPARATOR), $$2}'
+	          /^##:$$/ {printf "%s", section; section=$(HELP_SECTION_SEP)} \
+	          /^##:./ {printf "%s%s\n%s", section, $$2, $(HELP_SECTION_SEP); section=$(HELP_SECTION_SEP)} \
+	          /^[^#]/ {split($$1, targets, " "); for (i in targets) { printf "%s\033[36m%-$(HELP_WIDTH)s\033[0m%s%s\n", \
+	                   $(HELP_INDENT), $(HELP_PREFIX) targets[i], $(HELP_SEPARATOR), $$2;} section=$(HELP_SECTION_SEP) }'
