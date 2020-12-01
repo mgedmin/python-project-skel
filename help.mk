4205b4298d1eef588357c695bbafcc58291b0260
Marius Gedminas <marius@gedmin.as>
2020-12-01 20:17:34 +0200

    Prettier printing of target aliases
    This
    
      foo bar:   ##: do thing
    
    used to print:
    
      make foo    # do thing
      make bar    # do thing
    
    and now it will print
    
      make foo    # do thing
      make bar    # same as 'make foo'

--- help.mk
+++ help.mk
@@ -1,4 +1,4 @@
-# help.mk version 2.1 (2020-12-01)
+# help.mk version 2.2 (2020-12-01)
 #
 # Helpful Makefile rules for 'make help'
 # https://github.com/mgedmin/python-project-skel
@@ -9,6 +9,7 @@ HELP_PREFIX = "make "
 HELP_WIDTH = 24
 HELP_SEPARATOR = " \# "
 HELP_SECTION_SEP = "\n"
+HELP_SAME_AS = "same as '\033[36mmake %s\033[0m'"
 
 .PHONY: help
 help:
@@ -16,5 +17,9 @@ help:
 	    | awk 'BEGIN {FS = "(^|:[^#]*)##: "; section=""}; \
 	          /^##:$$/ {printf "%s", section; section=$(HELP_SECTION_SEP)} \
 	          /^##:./ {printf "%s%s\n%s", section, $$2, $(HELP_SECTION_SEP); section=$(HELP_SECTION_SEP)} \
-	          /^[^#]/ {split($$1, targets, " "); for (i in targets) { printf "%s\033[36m%-$(HELP_WIDTH)s\033[0m%s%s\n", \
-	                   $(HELP_INDENT), $(HELP_PREFIX) targets[i], $(HELP_SEPARATOR), $$2;} section=$(HELP_SECTION_SEP) }'
+	          /^[^#]/ {split($$1, targets, " "); desc=$$2; \
+	                   for (i in targets) { \
+	                     printf "%s\033[36m%-$(HELP_WIDTH)s\033[0m%s%s\n", $(HELP_INDENT), $(HELP_PREFIX) targets[i], $(HELP_SEPARATOR), desc; \
+	                     desc=sprintf($(subst ','"'"',$(HELP_SAME_AS)), targets[1]); \
+	                   } \
+	                   section=$(HELP_SECTION_SEP)}'
